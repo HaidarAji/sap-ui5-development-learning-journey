802f40cfd4eba22d1f7e22cce84fe0b55b6a38de
Haidar Prabowo <raden.prabowo@gojek.com>
2024-12-12 07:05:38 +0000

    Routing Back

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,14 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, History) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Detail", {
 
+            onNavBack: function () {
+                const sPreviousHash = History.getInstance().getPreviousHash();
+                console.log(sPreviousHash);
+
+                if (sPreviousHash !== undefined) {
+                   window.history.go(-1);
+                } else {
+                    const oRouter = this.getOwnerComponent().getRouter();
+                    oRouter.navTo("overview", {}, true);
+                    
+                }
+            }
 
 
         });
